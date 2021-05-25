aca81abad56bc1dc388eb1eceb092a67fbad9f15
Syahirahaar <48470854+Syahirahaar@users.noreply.github.com>
2021-05-25 10:32:15 +0800

    t1

--- try_streamlit.py
+++ try_streamlit.py
@@ -1,3 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 st.markdown('hoho')
+
+df = pd.read_csv('C:/Users/Syahirahar/Documents/GitHub/MyDataEngineeringProject/Invistico_Airline.csv').head(5)
+st.write(df)
